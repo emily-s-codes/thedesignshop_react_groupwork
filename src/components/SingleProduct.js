8f0571c1eb2c780918124892a52064ad34d5f20d
emilysupercode <sheil.emily.sc@gmail.com>
2022-12-11 15:52:02 +0100

    styling updated, images imported with require.context, todo: fix broken links and images, apply class based on natural image orientation

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,18 +1,60 @@
 import products from "./data.json";
 
 const SingleProduct = () => {
-    console.log(products[0])
+    function importAll(r) {
+        let images = [];
+        r.keys().map((item) => { return images[item.replace('./', '')] = r(item); });
+        return images;
+    }
+
+    //SYNTAX: variable declarieren mit Funktion require.context und 
+    //parameter = 
+    //directory (Pfad zum Ordner), 
+    //useSubdirectories (suche noch in unterordner? true oder false), 
+    //regular expression (alles das hier .png ODER .jpeg ODER .jpg ODER .svg enthält) 
+    const images = importAll(require.context('../assets/img', false, /.(png|jpe?g|svg)$/));
+    // let singleImage = images[i];
+    console.log(images)
+
+    // apply different class to portrait image and landscape image by comparing height and width INCOMPLETE
+
+    // function imageOrientation() {
+    //     for (i = 0; i < images.length; i++) {
+    //         singleImage = images[i];
+    //         if (images.complete) {
+    //             // The image is already loaded, call handler
+    //             checkImage(images);
+    //         } else {
+    //             // The image is not loaded yet, set load event handler
+    //             images.addEventListener("load", function () {
+    //                 checkImage(this);
+    //             })
+    //         }
+    //     }
+    // }
+
+    // imageOrientation(images);
+
+    // function checkImage(singleImage) {
+    //     if (singleImage.naturalHeight > singleImage.naturalWidth) {
+    //         singleImage.classList.add("portrait")
+    //     } else {
+    //         singleImage.classList.add("landscape")
+    //     }
+    // }
+
     return (
-        <div >
-            <p>test</p>
+        <div className="productsDiv">
             {products.map((product, index) => {
                 return (
-                    <div key={index} className="singleProductDiv">
-                        <p>{product.Product_name}</p>
+                    <div key={index} className="singleProductDiv" onClick={() => window.open(product.Link_shop)}>
+                        <img src={images[product.Product_picture]} alt={product.Product_name} />
+                        <p className="bold">{product.Product_name}</p>
+                        <p className="light">{product.Price}</p>
                     </div>
                 )
             })}
-        </div>
+        </div >
     );
 }
 
